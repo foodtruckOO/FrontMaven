f453cd216f179facdaef11fbef724bfb9f8009af
walter <mi@mi.net>
2018-03-08 11:48:03 +0900

    MAVEN

--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,8 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" contentType="text/html; charset=utf-8" %>
 
+<img src="<c:url value='/images/user.png'/>" alt="/images/user.png"/><br/>
+<div style="height: 50px; background-color: red"></div>
 
 <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
--- src/main/webapp/bootstrap/js/template.js
+++ src/main/webapp/bootstrap/js/template.js
@@ -1,4 +1,20 @@
 /*foodtruck js*/
+/**/
+	function popupOpen(){
+	var popUrl = "<c:url value='/login/Login.jsp'/>";	//팝업창에 출력될 페이지 URL
+	var popOption = "width=380, height=580, resizable=no, scrollbars=no, status=no, left=800, top=200, location=no;";    //팝업창 옵션(optoin)
+		window.open(popUrl,"",popOption);
+		Document.log("popupOpen");
+	} 
+	
+	function popupOpen2(){
+	var popUrl = "<c:url value='login/Join.jsp'/>";	//팝업창에 출력될 페이지 URL
+	var popOption = "width=650, height=500, resizable=no, scrollbars=no, status=no, left=600, top=200, location=no;";    //팝업창 옵션(optoin)
+		window.open(popUrl,"",popOption);
+	}
+
+
+/*right box 스크롤 따라다니기*/
 $(function(){
 	var currentPosition = parseInt($("#sidebox").css("top"));
 	$(window).scroll(function() {
